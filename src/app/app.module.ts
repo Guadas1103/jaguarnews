48f326e7f8f9e429a5acd7b5f8c7c613da08591a
Codeminer15 <136392287+Codeminer15@users.noreply.github.com>
2023-11-06 22:17:32 -0600

    Subir logo y crear carreras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { CarrerasComponent } from './carreras/carreras.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    CarrerasComponent
   ],
   imports: [
     BrowserModule,
