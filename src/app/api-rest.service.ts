9af035058f635c826f40fe7b307522f28f7e53d9
Codeminer15 <136392287+Codeminer15@users.noreply.github.com>
2023-11-27 02:10:35 -0600

    getAllNoticias

--- src/app/api-rest.service.ts
+++ src/app/api-rest.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, catchError } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -53,6 +55,8 @@ constructor(private http: HttpClient) { }
       return this.http.delete(this.url + "preguntas/"+id)
     }
 
-
+    getAllNoticias(){
+      return this.http.get<any>(this.url + 'noticias');
+    }
   
 }
--- src/app/carreras/sistema/sistema.component.ts
+++ src/app/carreras/sistema/sistema.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiRestService } from '../../api-rest.service';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -8,9 +9,40 @@ import { Router } from '@angular/router'
   templateUrl: './sistema.component.html',
   styleUrls: ['./sistema.component.css']
 })
-export class SistemaComponent {
+export class SistemaComponent implements OnInit{
+  noticias = [
+    {autor: '', titulo:'', descripcion:'', fecha:'', fechaModificacion:'', imagenUrl:''}
+  ]
+
   constructor(private api: ApiRestService, private router: Router) {}
-  ngOnInit():void {}
+  
+  ngOnInit():void {
+      this.consulta()
+  }
+
+  consulta(){
+    this.api.getAllNoticias().subscribe({
+      next: datos => {
+        //console.log(datos)
+        let i = 1;  
+        const documents = datos.documents.filter((p:any) => p.hasOwnProperty("fields"))
+        console.log(documents)
+        this.noticias = documents.map((p: {name: String, fields: any}) => (
+          {
+          autor: p.fields.hasOwnProperty('autor')? p.fields.autor.stringValue:"",
+          titulo: p.fields.hasOwnProperty('titulo')? p.fields.titulo.stringValue:"",
+          descripcion: p.fields.hasOwnProperty('descripcion')? p.fields.descripcion.stringValue:"",
+          fecha: p.fields.hasOwnProperty('fecha')? p.fields.timestampValue:"",
+          fechaModificacion: p.fields.hasOwnProperty('fechaModificacion')? p.fields.timestampValue:"",
+          imagenUrl: p.fields.hasOwnProperty('imagenUrl')? p.fields.imagenUrl.stringValue:"",
+        }))
+        console.log(this.noticias)
+      },
+      error: e => {
+        console.error("Error al obtener Noticias:", e);
+      }
+    })
+  }
 
   navigateToHome() {
   this.router.navigate(['/home']);
